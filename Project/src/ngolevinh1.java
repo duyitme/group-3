d9ae67f1b17bd1a20b9a01f546fa74ca6813124c
vinh <MY PC@LAPTOP-4V7O60GC>
2025-05-17 10:04:55 +0700

    cộng 2 số

---
+++ Project/src/ngolevinh1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ngolevinh1 {
+
+		public static void main(String[] args) {
+			 Scanner scanner = new Scanner(System.in);
+		        System.out.print("Nhập số thứ nhất: ");
+		        int a = scanner.nextInt();
+		        System.out.print("Nhập số thứ hai: ");
+		        int b = scanner.nextInt();
+		        int tong = a + b;
+		        System.out.println("Tổng của " + a + " và " + b + " là: " + tong);
+		    }
+		}
+		
+
